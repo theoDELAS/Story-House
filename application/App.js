15da179659c4de5cd61f6100c56c4d37035966d2
Théo <theo.delas@ynov.com>
2019-03-13 15:25:07 +0100

    test

--- application/App.js
+++ application/App.js
@@ -1,62 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Search from './components/adresse.js'
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <Search/>
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#E69B5D',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  logo:{
-
-  },
-  code_p: {
-
-  },
-  ville: {
-
-  },
-  rue: {
-
-  },
-  num: {
-
-  },
-  complement: {
-
-  },
-  rechercher: {
-
-  },
-  add: {
-
-  },
-});
